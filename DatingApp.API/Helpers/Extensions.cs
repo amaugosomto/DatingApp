8d718472a5a6fb2db8ad46141c6a4eb133492dbb
Somto C. Amaugo <amaugosomto@gmail.com>
2019-03-12 11:20:25 +0100

    Extending the API, added dummy data and DTOS

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingApp.API.Helpers
@@ -12,6 +13,16 @@ namespace DatingApp.API.Helpers
             // These other two allows the top header to be displayed
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        // Using "this" because we are adding an extension method to an existing class
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+                age--;
+
+            return age;
         }
     }
 }
